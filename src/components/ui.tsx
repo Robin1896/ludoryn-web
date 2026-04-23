d98946310e3d66650d792db08590296b51c18dea
Robin Zegers <robinzegers@livewallgroup.com>
2026-04-23 21:33:58 +0200

    Revert avatar to square

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -177,7 +177,7 @@ export function Avatar({
   return (
     <div style={{ position: "relative", flexShrink: 0, display: "inline-flex" }}>
       <div style={{
-        width: size, height: size, borderRadius: "50%", background: color,
+        width: size, height: size, borderRadius: 0, background: color,
         display: "flex", alignItems: "center", justifyContent: "center",
         fontSize: size * 0.38, fontWeight: 600, color: "#fff",
         fontFamily: "var(--font-body)",
